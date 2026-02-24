1edcda5bf55cfbba180797f710922adfd0ef1bbd
tf-preetham <51767050+tf-preetham@users.noreply.github.com>
2026-02-24 23:18:05 +1300

    Update xx_terminal.py

--- tabdiff/xx_terminal.py
+++ tabdiff/xx_terminal.py
@@ -1,9 +1,11 @@
+
+## Commands to run in the terminal
+
 # conda activate tabdiff
 # python main.py --dataname diabetes --mode train --no_wandb
 # python main.py --dataname diabetes --mode test --report --no_wandb
+# python main.py --dataname diabetes --mode train --y_only --no_wandb
+
 
 
 # python main.py --dataname adult --mode test --report --no_wandb --ckpt_path "C:\Users\Preetham_Arvind\Documents\Analytics\TabDiff\tabdiff\ckpt\adult\learnable_schedule\model_400.pt"
-
-import sys
-print(sys.path)
